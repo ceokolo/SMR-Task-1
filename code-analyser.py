1f5836f35c1e2f2fc28d157b90cf2eef2ffb1f04
ceokolo <okolocollinsc@gmail.com>
2019-01-13 20:02:02 +0100

    Task 1: Using gitpython to find only modified changes in commits

--- code-analyser.py
+++ code-analyser.py
@@ -9,13 +9,22 @@ for commit in all_commits:
     print(commit.message, commit.hexsha)
 
 i = 0
-while i < len(all_commits):
-    if i == len(all_commits) - 1:
-        break
-    print(repo.git.diff(all_commits[i+1], all_commits[i]))
+while i < len(all_commits) - 1:
+    diff = repo.git.diff(all_commits[i+1], all_commits[i])
+    diff_array = diff.split('\n')
+    j = 0
+    modified = []
+    while j < len(diff_array) - 1:
+        if diff_array[j].startswith('-') and diff_array[j + 1].startswith('+'):
+            modified.append(diff_array[j])
+            modified.append(diff_array[j + 1])
+            j += 2
+            continue
+        j += 1
+    print("\n".join(modified))
     """for change in all_commits[i].diff(all_commits[i + 1]).iter_change_type('M'):
         first = change.a_blob.data_stream.read().decode('utf-8')
         second = change.b_blob.data_stream.read().decode('utf-8')
         print(list(difflib.unified_diff(first, second)))"""
-    print("======================================================")
+    print("======================================================\n")
     i += 1
