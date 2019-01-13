ef27089802cc0936a0a70555a38a9a1216cbf29f
ceokolo <okolocollinsc@gmail.com>
2019-01-13 23:00:39 +0100

    Task 1: Combined all objects into a single class

--- code-analyser.py
+++ code-analyser.py
@@ -1,30 +1,122 @@
 from git import Repo
-from git.compat import defenc
-import difflib
+import pandas as pd
 import sys
+import os
+import re
 
-repo = Repo('.git')
-all_commits = list(repo.iter_commits('master'))
-for commit in all_commits:
-    print(commit.message, commit.hexsha)
 
-i = 0
-while i < len(all_commits) - 1:
-    diff = repo.git.diff(all_commits[i+1], all_commits[i])
-    diff_array = diff.split('\n')
-    j = 0
-    modified = []
-    while j < len(diff_array) - 1:
-        if diff_array[j].startswith('-') and diff_array[j + 1].startswith('+'):
-            modified.append(diff_array[j])
-            modified.append(diff_array[j + 1])
-            j += 2
-            continue
-        j += 1
-    print("\n".join(modified))
-    """for change in all_commits[i].diff(all_commits[i + 1]).iter_change_type('M'):
-        first = change.a_blob.data_stream.read().decode('utf-8')
-        second = change.b_blob.data_stream.read().decode('utf-8')
-        print(list(difflib.unified_diff(first, second)))"""
-    print("======================================================\n")
-    i += 1
+class FunctionChangeObserver():
+    def __init__(self, repository):
+        dir_array = repository.split('/')
+        dir_name = dir_array[len(dir_array) - 1]
+        self.csv_dict = {
+            "Commit SHA": [],
+            "Java File": [],
+            "Old Function Signature": [],
+            "New Function Signature": []
+        }
+        if os.path.isdir(dir_name):
+            self.repo = Repo(dir_name + '/.git')
+            print(list(self.repo.iter_commits('master')))
+        else:
+            repo = Repo('.git')
+            repo.git.clone(repository)
+            self.repo = Repo(dir_name + '/.git')
+            print(list(self.repo.iter_commits('master')))
+
+    def get_all_commits(self):
+        all_commits = list(self.repo.iter_commits('master'))
+        return all_commits
+
+    def process_all_commits(self):
+        all_commits = self.get_all_commits()
+        for index in range(len(all_commits) - 1):
+            old_commit = all_commits[index+1]
+            new_commit = all_commits[index]
+            modified_diff = self.get_modified_commit_diff(old_commit, new_commit)
+            if modified_diff is not None and len(modified_diff) > 0:
+                print(modified_diff)
+                functional_diff = self.get_functional_changes(modified_diff)
+                if len(functional_diff) > 0:
+                    self.add_to_csv(functional_diff, new_commit.hexsha)
+            print("======================================================\n")
+        df = pd.DataFrame(self.csv_dict)
+        df.to_csv('data.csv', index=None)
+
+    def get_modified_commit_diff(self, old_commit, new_commit):
+        diff = self.repo.git.diff(old_commit, new_commit)
+        diff_array = diff.split('\n')
+        index = 0
+        modified = []
+        while index < len(diff_array) - 1:
+            if diff_array[index].startswith('-') and diff_array[index + 1].startswith('+'):
+                modified.append(diff_array[index])
+                modified.append(diff_array[index + 1])
+                index += 2
+                continue
+            index += 1
+        if len(modified) > 0:
+            change_dict = self.convert_change_list_to_dict(modified)
+            only_java_changes = self.remove_non_java_and_empty_files(change_dict)
+        else:
+            return None
+        return only_java_changes
+
+    def convert_change_list_to_dict(self, modified_list):
+        changes_dict = {}
+        file_name = ""
+        for index in range(0, len(modified_list), 2):
+            old = modified_list[index]
+            new = modified_list[index+1]
+            if old.startswith('--- '):
+                file_name = old.split()[1][2:]
+                changes_dict[file_name] = []
+            else:
+                changes_dict[file_name].append(old)
+                changes_dict[file_name].append(new)
+        return changes_dict
+
+    def remove_non_java_and_empty_files(self, change_dict):
+        file_names = change_dict.keys()
+        new_dict = {}
+        for file in file_names:
+            if file.find('.java') > -1 and len(change_dict[file]) > 0:
+                new_dict[file] = change_dict[file]
+        return new_dict
+
+    def get_functional_changes(self, diff_dict):
+        new_dict = {}
+        for file_name in diff_dict.keys():
+            for index in range(0, len(diff_dict[file_name]), 2):
+                deletion = diff_dict[file_name][index]
+                addition = diff_dict[file_name][index + 1]
+                is_function = re.match('\sfunction\s\w+\(', deletion) and re.match('\sfunction\s\w+\(', addition)
+                if is_function and len(deletion.split(',')) < len(addition.split(',')):
+                    if file_name in new_dict:
+                        new_dict[file_name].append(deletion)
+                        new_dict[file_name].append(addition)
+                    else:
+                        new_dict[file_name] = [deletion, addition]
+        return new_dict
+
+    def add_to_csv(self, diff_dict, commit_hash):
+        for file_name in diff_dict.keys():
+            for index in range(0, len(diff_dict[file_name]), 2):
+                old = diff_dict[file_name][index]
+                new = diff_dict[file_name][index + 1]
+                old_signature = self.get_function_signature(old)
+                new_signature = self.get_function_signature(new)
+                self.csv_dict["Commit SHA"].append(commit_hash)
+                self.csv_dict["Java File"].append(file_name)
+                self.csv_dict["Old Function Signature"].append(old_signature)
+                self.csv_dict["New Function Signature"].append(new)
+
+    def get_function_signature(self, change):
+        index_of_the_string_function = change.find('function')
+        index_of_bracket_end = change.find(')')
+        signature = change[index_of_the_string_function + 8, index_of_bracket_end + 1]
+        return signature.strip()
+
+
+fc = FunctionChangeObserver("https://github.com/JakeWharton/RxRelay")
+fc.process_all_commits()
