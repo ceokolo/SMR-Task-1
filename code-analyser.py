2fa89c21f365cf225e17e8d31ec6c6108e487529
ceokolo <okolocollinsc@gmail.com>
2019-01-13 18:32:57 +0100

    Task 1: Using gitpython to check out the structure of the diff objects

--- code-analyser.py
+++ code-analyser.py
@@ -5,6 +5,12 @@ all_commits = list(repo.iter_commits('master'))
 for commit in all_commits:
     print(commit.message, commit.hexsha)
 
-for change in all_commits[0].diff(all_commits[1]).iter_change_type('M'):
-    print(change.a_blob.data_stream.read().decode('utf-8'))
-    print(change.b_blob.data_stream.read().decode('utf-8'))
+i = 0
+while i < len(all_commits):
+    if i == len(all_commits) - 1:
+        break
+    for change in all_commits[i].diff(all_commits[i + 1]).iter_change_type('M'):
+        #print(change.a_blob.data_stream.read().decode('utf-8'))
+        #print(change.b_blob.data_stream.read().decode('utf-8'))
+        print(change)
+    i += 1
