70087e197b3296e56ec06f1620b5695aa29f7500
ceokolo <okolocollinsc@gmail.com>
2019-01-13 19:30:48 +0100

    Task 1: Using gitpython to call git directly

--- code-analyser.py
+++ code-analyser.py
@@ -12,8 +12,10 @@ i = 0
 while i < len(all_commits):
     if i == len(all_commits) - 1:
         break
-    for change in all_commits[i].diff(all_commits[i + 1]).iter_change_type('M'):
+    print(repo.git.diff(all_commits[i+1], all_commits[i]))
+    """for change in all_commits[i].diff(all_commits[i + 1]).iter_change_type('M'):
         first = change.a_blob.data_stream.read().decode('utf-8')
         second = change.b_blob.data_stream.read().decode('utf-8')
-        print(difflib.unified_diff(first, second))
+        print(list(difflib.unified_diff(first, second)))"""
+    print("======================================================")
     i += 1
