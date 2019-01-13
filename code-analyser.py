17be55aa42938f726c8a3b65838d07f71eee62f9
ceokolo <okolocollinsc@gmail.com>
2019-01-13 18:49:25 +0100

    Task 1: Testing difflib to get diffs between files

--- code-analyser.py
+++ code-analyser.py
@@ -1,4 +1,7 @@
 from git import Repo
+from git.compat import defenc
+import difflib
+import sys
 
 repo = Repo('.git')
 all_commits = list(repo.iter_commits('master'))
@@ -10,7 +13,7 @@ while i < len(all_commits):
     if i == len(all_commits) - 1:
         break
     for change in all_commits[i].diff(all_commits[i + 1]).iter_change_type('M'):
-        #print(change.a_blob.data_stream.read().decode('utf-8'))
-        #print(change.b_blob.data_stream.read().decode('utf-8'))
-        print(change)
+        first = change.a_blob.data_stream.read().decode('utf-8')
+        second = change.b_blob.data_stream.read().decode('utf-8')
+        print(difflib.unified_diff(first, second))
     i += 1
